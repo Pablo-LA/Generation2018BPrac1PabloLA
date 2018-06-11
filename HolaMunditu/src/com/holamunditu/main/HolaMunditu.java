03c96d9f70fd998e3be692520a391b659e5e93f1
Pablo-LA <lopez.aguirre.pablo@gmail.com>
2018-06-11 16:47:14 -0500

    Segunda version de mi aplicacion HolaMunditu

--- HolaMunditu/src/com/holamunditu/main/HolaMunditu.java
+++ HolaMunditu/src/com/holamunditu/main/HolaMunditu.java
@@ -4,6 +4,8 @@ public class HolaMunditu {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Munditu!!");
+		System.out.println("Hasta la vista Munditu!!");
+		System.out.println("Hola de nuevo Munditu!!");
 
 	}
 
